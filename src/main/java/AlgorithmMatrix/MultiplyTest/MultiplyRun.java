1cb5eabe7cfa70ecb3c9aedf6d6dd44dcd045ec5
Anil Sevici <anilsevici@hotmail.com>
2015-05-19 17:34:53 +0300

    random dimension

--- src/main/java/AlgorithmMatrix/MultiplyTest/MultiplyRun.java
+++ src/main/java/AlgorithmMatrix/MultiplyTest/MultiplyRun.java
@@ -6,36 +6,64 @@ import java.util.Scanner;
 import AlgorithmMatrix.AlgorithmMatrixMultiplication.MultiplyAlgorithm;
 import AlgorithmMatrix.AlgorithmMatrixMultiplication.MultiplyDynamic;
 import AlgorithmMatrix.determine.Dimensions;
+import AlgorithmMatrix.generator.RandomMatrixGenerator;
 
 public class MultiplyRun {
 
 	public static void main(String[] args) {
 		final int NUM_OF_ITERATIONS = 5;
 		int total;
+		Random r = new Random();
 		Scanner in = new Scanner(System.in);
 
-		System.out.print("Number or matrices: Random or Manual?Random=Yes");
+		System.out.println("Number of matrices: Random or Manual?Random=Yes");
 		String input = in.nextLine();
 
 		if (input.equalsIgnoreCase("Yes")) {
-			Random r = new Random();
 			total = r.nextInt(5) + NUM_OF_ITERATIONS;
-			System.out.println("Multiply "+total+" matrices");
+			System.out.println("Multiply " + total + " matrices");
 		} else {
 			System.out.println("How many matrices would you like to multiply?");
 			total = in.nextInt();
 		}
 
-		Dimensions d = new Dimensions();
-		MultiplyAlgorithm dynamic = new MultiplyDynamic();
-		dynamic.multiply(d.determine_multiplication(total));
+		int p[] = new int[total + 1];
 
-		System.out.println("Optimal Parenthesization");
+		System.out
+				.println("Dimension of Matrices: Random or Manual?Random=Yes");
+		input = in.nextLine();
+
+		if (input.equalsIgnoreCase("Yes")) {
+			RandomMatrixGenerator generator = new RandomMatrixGenerator();
+			p = generator.generate(total);
+		} else {
+			Dimensions d = new Dimensions();
+			p = d.determine_multiplication(total);
+		}
+		
+		
+		matrixwithDimension(p, total);
+
+		MultiplyAlgorithm dynamic = new MultiplyDynamic();
+		dynamic.multiply(p);
+
+		System.out.println("\nOptimal Parenthesization");
 		System.out.println(dynamic.toString());
 
 		System.out.println("Minimum multiplication cost");
 		System.out.println(dynamic.getNumberOfMultiplication());
 
+	}
+	
+	public static void matrixwithDimension(int []p,int total_matrices)
+	{
+		System.out.print("[" + p[0] + "]");
+		for(int i=1;i<total_matrices+1;i++)
+		{
+			System.out.print("[" + p[i] + "]");
+			if(i<total_matrices)
+				System.out.print("x[" + p[i] + "]");
+		}
 	}
 
 }
--- src/main/java/AlgorithmMatrix/generator/RandomMatrixGenerator.java
+++ src/main/java/AlgorithmMatrix/generator/RandomMatrixGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomMatrixGenerator {
 
 	private Random random;
+	private static final int NUM_OF_ITERATIONS=5;
 
 	public RandomMatrixGenerator() {
 		random = new Random();
@@ -12,10 +13,11 @@ public class RandomMatrixGenerator {
 
 
 	public int[] generate(int n) {
-		int[] array = new int[n];
+		int[] array = new int[n+1];
 
+		array[0]=random.nextInt(95)+NUM_OF_ITERATIONS;
 		for (int i = 0; i < n; i++)
-			array[i] = random.nextInt();
+			array[i+1] = random.nextInt(95)+NUM_OF_ITERATIONS;
 
 		return array;
 	}
