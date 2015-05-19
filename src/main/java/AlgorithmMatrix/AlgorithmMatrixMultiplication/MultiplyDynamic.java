d0a235b41d1fdfcc3016e12bae446266b7f0c458
Anil Sevici <anilsevici@hotmail.com>
2015-05-19 16:51:50 +0300

    brute force is waiting

--- src/main/java/AlgorithmMatrix/AlgorithmMatrixMultiplication/MultiplyDynamic.java
+++ src/main/java/AlgorithmMatrix/AlgorithmMatrixMultiplication/MultiplyDynamic.java
@@ -12,31 +12,26 @@ public class MultiplyDynamic implements MultiplyAlgorithm {
 
 	@Override
 	public void multiply(int[] p) {
-		n = p.length - 1; // how many matrices are in the chain
-		m = new int[n + 1][n + 1]; // overallocate m, so that we don't use index
-									// 0
-		s = new int[n + 1][n + 1]; // same for s
-		matrixChainOrder(p); // run the dynamic-programming algorithm
+		n = p.length - 1;
+		m = new int[n + 1][n + 1];
+		s = new int[n + 1][n + 1];
+		matrixChainOrder(p);
 
 	}
 
 	private void matrixChainOrder(int[] p) {
-		// Initial the cost for the empty subproblems.
+
 		for (int i = 1; i <= n; i++)
 			m[i][i] = 0;
 
-		// Solve for chains of increasing length l.
 		for (int l = 2; l <= n; l++) {
 			for (int i = 1; i <= n - l + 1; i++) {
 				int j = i + l - 1;
 				m[i][j] = Integer.MAX_VALUE;
 
-				// Check each possible split to see if it's better
-				// than all seen so far.
 				for (int k = i; k < j; k++) {
 					int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
 					if (q < m[i][j]) {
-						// q is the best split for this subproblem so far.
 						m[i][j] = q;
 						s[i][j] = k;
 					}
